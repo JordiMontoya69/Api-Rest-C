363ff0a9738ea0b357d0d848ee13c0fca0c47527
JordiMontoya69 <114711603+JordiMontoya69@users.noreply.github.com>
2023-04-03 21:41:51 -0600

    Add files via upload

---
+++ ApiRest/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace ApiRest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbLongitud = new System.Windows.Forms.TextBox();
+            this.tbLatitud = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pbImagen = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Longitud";
+            // 
+            // tbLongitud
+            // 
+            this.tbLongitud.Location = new System.Drawing.Point(114, 147);
+            this.tbLongitud.Name = "tbLongitud";
+            this.tbLongitud.Size = new System.Drawing.Size(100, 20);
+            this.tbLongitud.TabIndex = 2;
+            // 
+            // tbLatitud
+            // 
+            this.tbLatitud.Location = new System.Drawing.Point(114, 109);
+            this.tbLatitud.Name = "tbLatitud";
+            this.tbLatitud.Size = new System.Drawing.Size(100, 20);
+            this.tbLatitud.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(44, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Latitud";
+            // 
+            // pbImagen
+            // 
+            this.pbImagen.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pbImagen.Location = new System.Drawing.Point(246, 21);
+            this.pbImagen.Name = "pbImagen";
+            this.pbImagen.Size = new System.Drawing.Size(487, 308);
+            this.pbImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImagen.TabIndex = 5;
+            this.pbImagen.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(87, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "C#";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 341);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pbImagen);
+            this.Controls.Add(this.tbLatitud);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbLongitud);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbLongitud;
+        private System.Windows.Forms.TextBox tbLatitud;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pbImagen;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ApiRest/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApiRest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DBApi datos = new DBApi();
+                dynamic resultados = datos.Get("https://api.nasa.gov/planetary/earth/assets?lon=" + tbLongitud.Text + "&lat=" + tbLatitud.Text + "&date=2018-01-01&&dim=0.10&api_key=hzk4Tt8dm0wsur8fdFFf8W7o8IAfzn6l2KnxTnwy");
+                pbImagen.ImageLocation = resultados.url;
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+    }
+}
